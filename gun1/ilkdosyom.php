939daca7d08b52c0ec3f2824cd19468215faa3a5
ugurdinc <dinc.ugur@gmail.com>
2015-08-08 19:51:41 +0300

    1. gün yapılanlar

---
+++ gun1/ilkdosyom.php
@@ -0,0 +1,18 @@
+<meta charset="utf-8"/>
+<?php
+$sayi1 = 5;
+$sayi2 = 3;
+
+//echo $sayi1 + $sayi2;
+//echo $sayi1 - $sayi2;
+//echo $sayi1 * $sayi2;
+
+//echo ceil($sayi1 / $sayi2);yukarı yuvarlama
+//echo floor($sayi1 / $sayi2);aşağı yuvarlama
+
+//echo $sayi1 % $sayi2;
+
+//echo ++$sayi1;
+
+
+?>
